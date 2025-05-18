2f1065424491b01fdebb1d78b2217f5a54ee95af
JhoselPR <112789135+JhoselPR@users.noreply.github.com>
2025-05-17 18:05:17 -0600

    feat: database connection

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import colors from 'colors';
 import server from './server';
 
 const port = process.env.PORT || 4000; //.PORT es variable de entorno que existe en todos los hosting que soportan Node.JS
 
 server.listen(port, () => {
-    console.log('Servidor funcionando en el puerto: ',4000);
+    console.log( colors.blue.bold(`Servidor funcionando en el puerto: ${4000}`));
 })
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 //const express = require('express'); //Common JS
 import express from 'express'; //ESM sintaxis
+import 'dotenv/config'; // Cargar variables de entorno
 import router from './router';
+import { connectDB } from './config/db';
+
 const app = express();
+connectDB(); // Conexión a la base de datos
 
-app.use('/', router); //.use : cada que hay una petición a la URL principal se va a ejecutar la función router y va a entrar a todos los métodos que tengamos, mapeando cada una de las URL
+app.use(express.json()); // Leer datos de formularios
 
+app.use('/', router); // .use : cada que hay una petición a la URL principal se va a ejecutar la función router y va a entrar a todos los métodos que tengamos, mapeando cada una de las URL
 
 
 export default app;
