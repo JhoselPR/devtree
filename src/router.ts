740912eb353a9a2dc48b42b0f3e26e011e8bec46
JhoselPR <112789135+JhoselPR@users.noreply.github.com>
2025-05-20 18:17:58 -0600

    feat: user register to db

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express';
+import User from './models/User';
 
 const router = Router();
 
 //* Routing
 
 /** Autenticación y registro **/
-router.post('/auth/register', (req, res) => {
+router.post('/auth/register', async (req, res) => {
     console.log(req.body);
+    const user = new User(req.body);
+    await user.save();
+
+    res.send('Usuario creado correctamente');
 })
 
 export default router;
