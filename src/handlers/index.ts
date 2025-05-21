4134b56a53b33dfc0b546a64defa9340df90a2cc
JhoselPR <112789135+JhoselPR@users.noreply.github.com>
2025-05-20 19:12:37 -0600

    feat: username

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,22 +1,32 @@
 import { Request, Response } from "express";
+import slug from "slug";
 import User from "../models/User";
-import { hash } from "bcrypt";
 import { hashPassword } from "../utils/auth";
 
-export const createAccount = async (req : Request, res : Response) => {
+export const createAccount = async (req: Request, res: Response) => {
 
-    const { email, password  } = req.body;
+    const { email, password } = req.body;
 
     const userExists = await User.findOne({ email });
     if (userExists) {
-        const error = new Error('El usuario ya está registrado');
-        res.status(409).json({error : error.message});
+        const error = new Error('El email ya está registrado');
+        res.status(409).json({ error: error.message });
+        return
+    }
+
+    const handle = slug(req.body.handle, '');
+    const handleExists = await User.findOne({ handle });
+    if (handleExists) {
+        const error = new Error('Nombre de usuario no disponible');
+        res.status(409).json({ error: error.message });
         return
     }
 
     const user = new User(req.body);
     user.password = await hashPassword(password)
-    await user.save();
+    user.handle = handle;
 
+
+    await user.save();
     res.status(201).send('Usuario creado correctamente');
 }
