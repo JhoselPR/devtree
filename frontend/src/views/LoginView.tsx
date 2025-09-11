13f0a5b283237e83df0f85dc0b1c8953ad1788e4
JhoselPR <112789135+JhoselPR@users.noreply.github.com>
2025-09-11 14:19:09 -0600

    feat: layout y views añadidas

---
+++ frontend/src/views/LoginView.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+export default function LoginView() {
+  return (
+    <>
+    <h1 className='text-4xl text-white font-bold'>Iniciar sesión</h1>
+
+      <nav className="mt-10">
+        <Link
+          to="/auth/register"
+          className="text-center text-white text-lg block"
+        >
+          ¿No tienes cuenta? Regístrate
+        </Link>
+      </nav>
+    </>
+  );
+}
